8d42c59051f00f5bb6294e16ae628e320b7d65b4
LMM07 <mm_0028@naver.com>
2022-03-25 16:14:04 +0900

    server

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -326,7 +326,7 @@ export default {
         },
         loginWithKakao() {
             if (window.Kakao.isInitialized() == false) {
-                window.Kakao.init('255b187f87731368f5e47c3310b3cf02');
+                window.Kakao.init('97a17f9847fd0a59bd966f2602972995');
             }
             window.Kakao.Auth.login({
                 success: this.kakaoLoginCheck(),
@@ -426,7 +426,7 @@ export default {
         else
             this.setPath('/')
 
-        const naver_id_login = new window.naver_id_login("INmTkpuK5mPhbhHfYG_Q", "http://118.67.128.121:8085/authentication/signIn/naver");
+        const naver_id_login = new window.naver_id_login("j2LwMSzFsfRp3s5lJ5e1", "http://118.67.128.140:8085/authentication/signIn/naver");
         let link = document.location.href;
         if (link.indexOf('access_token') != -1) {
             this.naverLogin(naver_id_login.getAccessToken());
